7c6ff2ff9ede1dc52cfb56548ddcb9f95964d317
Aless Tosi <tosialessandro002@gmail.com>
2025-09-12 01:39:29 +0200

    feat(family data model): first draft of family and person data model in python. translate existing data model and update to follow clean code good practices

---
+++ python-app/src/Family.py
@@ -0,0 +1,40 @@
+# Family.py
+
+from typing import List, Optional
+from dataclasses import dataclass, field
+from datetime import date
+from enum import Enum
+
+from Event import Event
+
+class RelationKind(Enum):
+    MARRIED = "married"
+    NOT_MARRIED = "not_married"
+    PACS = "pacs"
+    ENGAGED = "engaged"
+    SEPARATED = "separated"
+    DIVORCED = "divorced"
+    UNKNOWN = "unknown"
+
+@dataclass
+class Family:
+    parent1: int = 0
+    parent2: int = 0
+    children: List[int] = field(default_factory=list) # List of unique identifiers of children
+    relation: RelationKind = RelationKind.UNKNOWN
+
+    marriage: Optional['Event'] = None
+    divorce: Optional['Event'] = None
+    events: List['Event'] = field(default_factory=list)
+
+    notes: Optional[str] = None
+    origin_file: Optional[str] = None
+    sources: Optional[str] = None
+    key_index: Optional[int] = None  # Unique identifier for the family
+
+    def add_child(self, index: int):
+        if index not in self.children:
+            self.children.append(index)
+
+    def add_event(self, event: Event):
+        self.events.append(event)
